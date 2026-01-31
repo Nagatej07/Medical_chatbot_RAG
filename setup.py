3abcc14c2122b93a2e8e713fb65c649f8bd76d2f
Nagateja07 <naga.teja.hrr@gmail.com>
2026-01-31 16:41:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Bingi Nagateja",
+    author_email="binginagateja@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
